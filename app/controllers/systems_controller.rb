255776a532d1c7401dc6e2e3f82fef89c8930720
Alexandr Masterov <mailforzftsh@gmail.com>
2016-05-14 22:51:59 +0300

    Making some veiws, starting systems develpment

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,2 +1,27 @@
 class SystemsController < ApplicationController
+
+	def index
+		@systems = System.all
+	end
+
+	def show
+		@system = System.find_by_id(params[:id])
+	end
+
+	def new
+		@system = System.new
+	end
+
+	def create
+		@image = System.new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end

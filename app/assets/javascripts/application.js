764f3dbee1a1e644d6a298663190ed9c324c335c
Alexandr Masterov <mailforzftsh@gmail.com>
2016-05-18 15:17:00 +0300

    Deploying on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require bootstrap-sprockets
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap-sprockets
-//= require bootstrap
 //= require bootsy
